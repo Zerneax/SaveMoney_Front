1c3642643379aae7c496a77ccee69fb9f344c124
Zerneax <d.chris59@gmail.com>
2018-11-09 09:58:02 +0100

    Delete useless import

--- src/main.js
+++ src/main.js
@@ -3,15 +3,10 @@ import App from './App.vue'
 import router from './router'
 import SuiVue from 'semantic-ui-vue'
 import VueMomentLib from 'vue-moment-lib'
-//import Moment from 'moment'
 
 import 'semantic-ui-css/semantic.min.css'
 
 Vue.use(SuiVue);
-
-
-// var moment = require('moment');
-// Vue.use(moment);
 Vue.use(VueMomentLib);
 
 Vue.config.productionTip = false
